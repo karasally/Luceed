dde50f547cc3302e730d6e3e1aafb67d845aa563
Kara Sally <karasally06@gmail.com>
2023-11-11 19:21:12 +0000

    Add project files.

---
+++ Luceed/LuceedForm.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace Luceed
+{
+    partial class LuceedForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            tabControl1 = new TabControl();
+            tabPage1 = new TabPage();
+            dataGridView1 = new DataGridView();
+            id = new DataGridViewTextBoxColumn();
+            naziv = new DataGridViewTextBoxColumn();
+            btnGetByNaziv = new Button();
+            tabPage2 = new TabPage();
+            tabControl1.SuspendLayout();
+            tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            tabControl1.Controls.Add(tabPage1);
+            tabControl1.Controls.Add(tabPage2);
+            tabControl1.Location = new Point(12, 12);
+            tabControl1.Name = "tabControl1";
+            tabControl1.SelectedIndex = 0;
+            tabControl1.Size = new Size(332, 300);
+            tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            tabPage1.Controls.Add(dataGridView1);
+            tabPage1.Controls.Add(btnGetByNaziv);
+            tabPage1.Location = new Point(4, 29);
+            tabPage1.Name = "tabPage1";
+            tabPage1.Padding = new Padding(3);
+            tabPage1.Size = new Size(324, 267);
+            tabPage1.TabIndex = 0;
+            tabPage1.Text = "tabPage1";
+            tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Columns.AddRange(new DataGridViewColumn[] { id, naziv });
+            dataGridView1.Location = new Point(6, 6);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.ReadOnly = true;
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.RowTemplate.Height = 29;
+            dataGridView1.Size = new Size(308, 202);
+            dataGridView1.TabIndex = 1;
+            // 
+            // id
+            // 
+            id.HeaderText = "ID";
+            id.MinimumWidth = 6;
+            id.Name = "id";
+            id.ReadOnly = true;
+            id.Width = 125;
+            // 
+            // naziv
+            // 
+            naziv.HeaderText = "Naziv";
+            naziv.MinimumWidth = 6;
+            naziv.Name = "naziv";
+            naziv.ReadOnly = true;
+            naziv.Width = 125;
+            // 
+            // btnGetByNaziv
+            // 
+            btnGetByNaziv.Location = new Point(96, 223);
+            btnGetByNaziv.Name = "btnGetByNaziv";
+            btnGetByNaziv.Size = new Size(125, 29);
+            btnGetByNaziv.TabIndex = 0;
+            btnGetByNaziv.Text = "Get By Naziv";
+            btnGetByNaziv.UseVisualStyleBackColor = true;
+            btnGetByNaziv.Click += btnGetByNaziv_Click;
+            // 
+            // tabPage2
+            // 
+            tabPage2.Location = new Point(4, 29);
+            tabPage2.Name = "tabPage2";
+            tabPage2.Padding = new Padding(3);
+            tabPage2.Size = new Size(768, 393);
+            tabPage2.TabIndex = 1;
+            tabPage2.Text = "tabPage2";
+            tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // LuceedForm
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(355, 321);
+            Controls.Add(tabControl1);
+            Name = "LuceedForm";
+            Text = "Form1";
+            tabControl1.ResumeLayout(false);
+            tabPage1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private TabControl tabControl1;
+        private TabPage tabPage1;
+        private TabPage tabPage2;
+        private Button btnGetByNaziv;
+        private DataGridView dataGridView1;
+        private DataGridViewTextBoxColumn id;
+        private DataGridViewTextBoxColumn naziv;
+    }
+}
---
+++ Luceed/LuceedForm.cs
@@ -0,0 +1,16 @@
+namespace Luceed
+{
+    public partial class LuceedForm : Form
+    {
+        public LuceedForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGetByNaziv_Click(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
